5aef64e62fafe3fea28166bf041d8460e23aaf58
Mark <mark.litfin@yahoo.com>
2020-05-07 06:30:55 -0400

    added API information

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-function buildQueryURL() {
+$(document).ready(function () {
+  function getWeatherData() {
+    var city = $("#search-term").val();
+
     // queryURL is the url we'll use to query the API
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=";
-    api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-  
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2aef98083e4917b47f111adf6ac2ab7c`;
+
     // Begin building an object to contain our API call's query parameters
     // Set the API key
-    var queryParams = { "api-key": "2aef98083e4917b47f111adf6ac2ab7c" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
+    $.get(queryURL, function (Response) {
+      console.log(Response);
+      console.log(Response.main.temp);
 
-      .val()
-      .trim();
+    })
+  }
+  $("#search-btn").on("click", function (event) {
+    event.preventDefault();
+    getWeatherData();
+
+  })
+})
+
